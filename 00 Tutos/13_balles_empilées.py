91d0a553737538e5aa78a2270df03edc600fc2f7
Julien Hilaire <julien.hilaire@imelmi.net>
2025-08-01 11:09:28 +0200

    v1.0 : fin = prêt pour la démonstration

--- 00 Tutos/13_balles_empilées.py
+++ 00 Tutos/13_balles_empilées.py
@@ -3,105 +3,150 @@ from manim.utils.space_ops import rotate_vector
 
 class CercleSurArc(Scene):
     def construct(self):
-        rayon = 1
+        rayon = 1.3
+        Line.set_default(stroke_width=2)
+        Circle.set_default(stroke_width=2)
+        Dot.set_default(stroke_width=1)
+        DashedLine.set_default(stroke_width=2)
+        standard_color = WHITE
+        self.all_objs = VGroup()  # Groupe global pour zoom final
 
-        # Création des cercles
-        centre = LEFT * rayon
-        cercle1 = Circle(radius=rayon, color=WHITE).move_to(centre)
-        cercle2 = Circle(radius=rayon, color=WHITE).next_to(cercle1, RIGHT, buff=0)
-        cercle3 = Circle(radius=rayon, color=WHITE).next_to(cercle1, UP, buff=0)
+        # ---- CERCLE 0 (référence grise, en arrière-plan) ----
+        cercle0 = DashedVMobject(Circle(radius=rayon, color=GREY), dashed_ratio=0.5, num_dashes=60).shift(UP*1.3 + LEFT)
+        cercle0.z_index = -1
+        cercle0.set_opacity(0)
+        centre0 = Dot(color=GREY, radius=0.03).move_to(cercle0.get_center())
+        self.add(cercle0, centre0)
+        self.all_objs.add(cercle0, centre0)
 
-        # Brace 1
-        brace1 = Brace(cercle1, direction=LEFT, buff=0.2, color=WHITE)
-        brace1_text = brace1.get_text("1").set_color(WHITE)
+        # ---- CERCLE 1 ----
+        cercle1 = Circle(radius=rayon, color=standard_color).shift(UP*1.3 + LEFT)
+        centre1 = Dot(color=standard_color, radius=0.03).move_to(cercle1.get_center())
+        self.add(centre1)
+        self.all_objs.add(cercle1, centre1)
 
-        # Brace 2
-        brace2 = Brace(cercle2, direction=DOWN, buff=0.2, color=WHITE)
-        brace2_text = brace2.get_text("1").set_color(WHITE)
+        # ---- CERCLE 2 ----
+        cercle2 = Circle(radius=rayon, color=standard_color).next_to(cercle1, DOWN, buff=0)
+        centre2 = Dot(color=standard_color, radius=0.03).move_to(cercle2.get_center())
+        self.add(centre2)
+        self.all_objs.add(cercle2, centre2)
 
-        # Brace 3 + texte + groupe + updater propre
-        brace3 = Brace(cercle3, direction=LEFT, buff=0.2, color=RED)
-        brace3_text = brace3.get_text("1").set_color(RED)
-        brace3_group = VGroup(brace3, brace3_text)
-        # Updater du groupe : suit le Y de cercle3, X fixe
-        brace3_group_x = brace3_group.get_x()
-        brace3_group.add_updater(lambda g: g.move_to([brace3_group_x, cercle3.get_y(), 0]))
+        # ---- CERCLE 3 ----
+        cercle3 = Circle(radius=rayon, color=standard_color).next_to(cercle2, RIGHT, buff=0)
+        centre3 = Dot(color=standard_color, radius=0.03).move_to(cercle3.get_center())
+        #centre3.add_updater(lambda d: d.move_to(cercle3.get_center()))
+        self.add(centre3)
+        self.all_objs.add(cercle3, centre3)
 
-        # Création des cercles
-        self.play(Create(cercle1), Create(cercle2), Create(cercle3))
-        self.wait(0.5)
-        #self.play(cercle3.animate.set_color(RED))
-        #self.wait(0.5)
+        # ---- BRACES ----
+        brace0 = VGroup(
+        Brace(cercle0, direction=LEFT, buff=0.2, color=standard_color),
+        Brace(cercle0, direction=LEFT, buff=0.2, color=standard_color).get_text("1").set_color(standard_color)
+        )
+
+        brace1 = always_redraw(lambda: VGroup(
+        (b := Brace(cercle1, direction=LEFT, buff=0.2, color=standard_color)),
+         b.get_text("1").set_color(standard_color)
+        ))
+
+        brace2 = VGroup(
+        Brace(cercle2, direction=LEFT, buff=0.2, color=standard_color),
+        Brace(cercle2, direction=LEFT, buff=0.2, color=standard_color).get_text("1").set_color(standard_color)
+        )
+
+        brace3 = VGroup(
+        Brace(cercle3, direction=DOWN, buff=0.2, color=standard_color),
+        Brace(cercle3, direction=DOWN, buff=0.2, color=standard_color).get_text("1").set_color(standard_color)
+        )
+        self.all_objs.add(brace1, brace2, brace3)
+
+        # ---- APPARITION ----
         self.play(
-            cercle3.animate.set_color(RED),
-            FadeIn(brace1_text), GrowFromCenter(brace1),
-            FadeIn(brace2_text), GrowFromCenter(brace2),
-            FadeIn(brace3_group)
+            Create(cercle1),
+            Create(cercle2),
+            Create(cercle3),
+            cercle0.animate.set_opacity(1),
+            run_time=1.5
+        )
+        self.wait(0.2)
+        centre1.add_updater(lambda d: d.move_to(cercle1.get_center()))
+        centre2.add_updater(lambda d: d.move_to(cercle2.get_center()))
+        centre3.add_updater(lambda d: d.move_to(cercle3.get_center()))
+        
+        self.play(
+            FadeIn(brace0),
+            FadeIn(brace1),
+            FadeIn(brace2),
+            FadeIn(brace3)
         )
         self.wait(0.5)
 
-        # Définition des arcs
-        arc_path = Arc(
-            radius=2 * rayon,
-            angle=-PI / 6,
-            start_angle=PI / 2,
-            arc_center=cercle1.get_center()
+        # ---- ANIMATION SUR ARC ----
+        arc_path = Arc(radius=2 * rayon, angle=-PI / 6, start_angle=PI / 2, arc_center=cercle2.get_center())
+        arc_path_back = Arc(radius=2 * rayon, angle=PI / 6, start_angle=PI / 3, arc_center=cercle2.get_center())
+
+        self.play(MoveAlongPath(cercle1, arc_path), run_time=2)
+        self.wait()
+        self.play(MoveAlongPath(cercle1, arc_path_back), run_time=2)
+        self.wait()
+
+        # ---- ON MASQUE LES ACCOLADES
+        self.play(
+            FadeOut(brace0),
+            FadeOut(brace1),
+            FadeOut(brace2),
+            FadeOut(brace3)
         )
-        arc_path_back = Arc(
-            radius=2 * rayon,
-            angle=PI / 6,
-            start_angle=PI / 3,
-            arc_center=cercle1.get_center()
-        )
+        self.wait()
 
-        self.play(MoveAlongPath(cercle3, arc_path), run_time=2, rate_func=smooth)
-        self.wait(1)
-        self.play(MoveAlongPath(cercle3, arc_path_back), run_time=2, rate_func=smooth)
-        self.wait(1)
-        self.play(MoveAlongPath(cercle3, arc_path), run_time=2, rate_func=smooth)
-        self.wait(1)
+        # ---- SEGMENTS DYNAMIQUES ----
+        segment1 = always_redraw(lambda: Line(cercle1.get_center(), cercle2.get_center(), color=standard_color))
+        segment2 = always_redraw(lambda: Line(cercle2.get_center(), cercle3.get_center(), color=standard_color))
+        segment3 = always_redraw(lambda: Line(cercle3.get_center(), cercle1.get_center(), color=standard_color))
+        self.all_objs.add(segment1, segment2, segment3)
+        self.wait()
 
-        # Zoom final
-        all_objs = VGroup(cercle1, cercle2, cercle3, brace1, brace1_text, brace2, brace2_text, brace3, brace3_text)
-        self.play(all_objs.animate.scale(2.5).move_to(ORIGIN), run_time=2)
-        self.wait(1)
+        # ---- REPERES DYNAMIQUES ----
+        dashedLine_cercle0 = always_redraw(lambda: DashedLine(
+            cercle0.get_center() - RIGHT * 2,
+            cercle0.get_center() + RIGHT * 4,
+            color=GREY
+        ))
+        dashedLine_cercle1 = always_redraw(lambda: DashedLine(
+            cercle1.get_center() - RIGHT * 3,
+            cercle1.get_center() + RIGHT * 3,
+            color=standard_color
+        ))
 
-        
-        # Ligne horizontale depuis la base de brace3 (vers la droite)
-        ligne_brace3 = DashedLine(
-            brace3.get_bottom() - RIGHT * 1,
-            brace3.get_bottom() + RIGHT * 12,
-            color=RED
-        )
-
-        # Ligne horizontale depuis le haut de brace2 (vers la droite)
-        ligne_brace1 = DashedLine(
-            brace1.get_top() - RIGHT * 1,
-            brace1.get_top() + RIGHT * 12,
-            color=WHITE
-        )
-        self.play(Create(ligne_brace3), Create(ligne_brace1))
-        self.wait(1)
-
-        # Position X de la flèche : légèrement avant la fin des dashlines
+        # ---- DOUBLE ARROW ----
         x_pos = cercle3.get_center()[0]
+        delta = 0.2
+        def double_fleche_redraw ():
+            start_point = [x_pos + delta, dashedLine_cercle0.get_y(), 0]
+            end_point   = [x_pos + delta, dashedLine_cercle1.get_y(), 0]
+            da = DoubleArrow(start=start_point, end=end_point, buff=0, color=standard_color, tip_length=0.2) 
+            return da
+        double_fleche = always_redraw(lambda: double_fleche_redraw () )
 
-        # Points de départ et d’arrivée
-        start_point = [x_pos, ligne_brace1.get_y(), 0]
-        end_point   = [x_pos, ligne_brace3.get_y(), 0]
+        #-----------------------
+        self.play(Create(dashedLine_cercle0), Create(dashedLine_cercle1), GrowArrow(double_fleche))
+        # ---- Déplacement final de cercle1 seul ----
+        self.play(MoveAlongPath(cercle1, arc_path), run_time=2)
+        # ------
+        label_x = MathTex("x").next_to(double_fleche, RIGHT, buff=0.1).set_color(standard_color)
+        self.play(FadeIn(label_x))
+        self.all_objs.add(dashedLine_cercle0, dashedLine_cercle1, double_fleche, label_x)
+        self.wait(4)
 
-        # Ligne verticale avec flèche
-        ligne_verticale = DoubleArrow(
-            start=start_point,
-            end=end_point,
-            buff=0,
-            stroke_width=2,
-            color=YELLOW,
-            tip_length=0.2
-        )
+        # On met tout en gris pour préparer la solution
+        standard_color = GREY
+        self.all_objs_solution = VGroup().add(cercle0,cercle1,cercle2,cercle3,centre0,centre1,centre2,centre3,dashedLine_cercle0,dashedLine_cercle1)
+        self.play(self.all_objs_solution.animate.set_color(standard_color), run_time=1.5)
+        # ---- ZOOM CENTRAL ----
+        #self.play(self.all_objs.animate.scale(2.5).move_to(ORIGIN), run_time=2)
+        #self.wait()
 
-        # Légende "x"
-        label_x = MathTex("x").next_to(ligne_verticale, RIGHT, buff=0.1).set_color(YELLOW)
-        # Animation de tracé de la ligne (comme un "expand")
-        self.play(GrowArrow(ligne_verticale), FadeIn(label_x))
-        self.wait(6)
+        # ---- DÉZOOM FINAL ----
+        self.wait(2)
+        #self.play(self.all_objs.animate.scale(0.4).move_to(ORIGIN), run_time=2)
+        self.wait(4)
